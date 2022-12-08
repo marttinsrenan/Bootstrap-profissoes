38b44b06075b8fd96ce8917cba47679a91171020
Renan Martins1 <99751865+marttinsrenan@users.noreply.github.com>
2022-12-07 23:44:50 -0300

    versão 1.0

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,7 +1,7 @@
 //Criar uma variável para manipular o XML
 xmlhttp = new XMLHttpRequest();
 //Efetuar a leitura do arquivo XML
-xmlhttp.open("GET","xml/conteudo.xml",false);
+xmlhttp.open("GET","xml/db_conteudo.xml",false);
 //Enviar o arquivo para leitura
 xmlhttp.send();
 //Informar que o arquivo é um XML
@@ -11,14 +11,39 @@ x = xmlDoc.getElementsByTagName("postagem");
 
 function funcaoConteudo(){
     for(n=x.length-1;n>=0;n--){
-            document.write("<tr>" +
-            "<td>"+ x[n].getAttribute("codigo") +"</td>" +
-            "<td><a href='postagem.html?noticia="+ n +"'>"+ x[n].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</a></td>" +
-            "<td>"+ x[n].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,250) + "...</td>" +
-            "<td><img src='imgs/"+ x[n].getElementsByTagName("imagem_1")[0].childNodes[0].nodeValue + "' width='250'></td>" +
-            "</tr>");
+            document.write("<div>" +
+             
+            "<div><a href='postagem.html?noticia="+ n +"'>"+ x[n].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</a></div>" +
+            "<div>"+ x[n].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,250) + "...</div>" +
+            "<div id='img'><img src='imgs/"+ x[n].getElementsByTagName("imagem_1")[0].childNodes[0].nodeValue + "' width='250'></div>" +
+            "</div>");
     }
 }
+
+function funcaoConteudo(){
+    for(n = 0; n < x.length; n++){
+        document.write(
+        "<div class='card mb-3 reveal bg-dark' id='noticia'>"+
+            "<div class='row no-gutters'>"+
+                "<div class='col-md-3 justify-content-center align-items-center d-flex'>"+
+                    "<img src='imgs/moto/"+ x[n].getElementsByTagName("imagem_1")[0].childNodes[0].nodeValue + "' style='max-height: 300px;' class='card-img'>" + 
+                "</div>" +
+                "<div class='col-md-8'>" + 
+                    "<div class='card-body'>"+
+                        "<h4 class='card-title'><a href='postagem.html?noticia=" + n + "' class='text-white' style='text-decoration:none;'>" + x[n].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</a></h5>"+
+                        "<p class='card-text'>" + x[n].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,245) +"...</p>" +
+                        "<a href='postagem.html?noticia=" + n + "' class='btn btn-primary'>Saiba Mais</a>"+
+                    "</div>" + 
+                "</div>" +
+            "</div>" +
+        "</div>");
+    }
+}
+
+
+
+
+
 
 function funcaoPostagem(){
     //Armazena a URL completa, ex:http://127.0.0.1/web-xml/postagem.html?noticia=0
@@ -28,10 +53,10 @@ function funcaoPostagem(){
     //Armazena somente o valor parâmetro da URL, ex: 0
     n = parametro.get("noticia");
    
-        document.write("<tr>" +
-        "<td>"+ x[n].getAttribute("codigo") +"</td>" +
-        "<td>"+ x[n].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</td>" +
-        "<td>"+ x[n].getElementsByTagName("corpo")[0].childNodes[0].nodeValue + "</td>" +
-        "<td><img src='imgs/"+ x[n].getElementsByTagName("imagem_1")[0].childNodes[0].nodeValue + "' width='250'></td>" +
-            "</tr>");    
+        document.write("<div>" +
+
+        "<div>"+ x[n].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</div>" +
+        "<div>"+ x[n].getElementsByTagName("corpo")[0].childNodes[0].nodeValue + "</div>" +
+        "<div><img src='imgs/"+ x[n].getElementsByTagName("imagem_1")[0].childNodes[0].nodeValue + "' width='250'></div>" +
+            "</div>");    
 }
